f750c1158b1e3834686dcf3cd0fafe0c6814e49f
Susan Tan <ssusantan9@gmail.com>
2019-04-11 21:13:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location 
       t.integer :costume_inventory 
-      t.integer :employees_count
+      t.integer :employee_count
       t.string :in_business
       t.timestamp :opening_time
       t.timestamp :closing_time

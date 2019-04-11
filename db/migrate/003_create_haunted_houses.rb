0f1890f66830102e90b0fbd71105f2e8c6c6b463
Susan Tan <ssusantan9@gmail.com>
2019-04-11 21:18:06 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,9 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+  
+  def change 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      

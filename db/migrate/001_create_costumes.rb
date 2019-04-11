9673e4179e8689095fdc5eaa3cb8db87b8ae0390
Susan Tan <ssusantan9@gmail.com>
2019-04-11 21:06:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       create_table :costumes do |t|
         t.string :name
         t.integer :price
-        t.size :integer
+        t.string :size
         t.string :image_url
         t.timestamp :created_at
         t.timestamp :updated_at
